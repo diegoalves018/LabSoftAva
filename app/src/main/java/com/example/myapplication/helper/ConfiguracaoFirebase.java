2bdec9b5097567bc4b748a5e14a44c6d10803c57
Diego <diegoalvesupis@gmail.com>
2021-03-10 20:51:15 -0300

    Firt Commit

---
+++ app/src/main/java/com/example/myapplication/helper/ConfiguracaoFirebase.java
@@ -0,0 +1,38 @@
+package com.example.myapplication.helper;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+
+public class ConfiguracaoFirebase {
+
+    private static DatabaseReference refereciaFirebase;
+    private static FirebaseAuth referenciaAutenticacao;
+    private static StorageReference referenciaStorage;
+
+
+
+
+    public static DatabaseReference getFirebase(){
+        if (refereciaFirebase == null){
+            refereciaFirebase = FirebaseDatabase.getInstance().getReference();
+        }
+        return refereciaFirebase;
+    }
+
+    public static FirebaseAuth getFirebaseAutenticacao(){
+        if (referenciaAutenticacao == null){
+            referenciaAutenticacao = FirebaseAuth.getInstance();
+        }
+        return referenciaAutenticacao;
+    }
+
+    public static StorageReference getFirebaseStorage(){
+        if (referenciaStorage == null){
+            referenciaStorage = FirebaseStorage.getInstance().getReference();
+        }
+        return referenciaStorage;
+    }
+}
